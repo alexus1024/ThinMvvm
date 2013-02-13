0359034dc7a0d280a3166e77ee9d9adf217acca7
alexus1024 <alexusr@nm.ru>
2013-02-13 13:11:49 +0600

    First

---
+++ ThinMvvmClient/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Alexus.ThinMvvmContract;
+
+namespace ThinMvvmClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var factory = new DuplexChannelFactory<IService>(typeof (CallbackImplementation), "ClientConne");
+
+            var channel = factory.CreateChannel();
+
+            channel.GetFirstClientModel(1, "Hi");
+        }
+    }
+
+    public class CallbackImplementation: IServiceCallback
+    {
+        public void Event(List<ServiceEvent> events)
+        {
+            
+        }
+    }
+}
---
+++ ThinMvvmContract/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alexus.ThinMvvmContract
+{
+    [ServiceContract(CallbackContract = typeof(IServiceCallback))]
+    public interface IService
+    {
+        FirstClientModel GetFirstClientModel(Int32 arg1, String arg2);
+    }
+
+    public interface IServiceCallback
+    {
+        void Event(List<ServiceEvent> events);
+    }
+
+    public class ServiceEvent
+    {
+    }
+
+    public class FirstClientModel
+    {
+        public Int32 Id { get; set; }
+        public String Data { get; set; }
+    }
+}
