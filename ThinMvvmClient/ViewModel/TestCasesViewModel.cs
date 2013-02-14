3dbd910b8c840badb6f0234e716248d7cfa44009
alexus1024 <alexusr@nm.ru>
2013-02-14 18:13:37 +0600

    + test cases navigation

--- ThinMvvmClient/ViewModel/TestCasesViewModel.cs
+++ ThinMvvmClient/ViewModel/TestCasesViewModel.cs
@@ -1,13 +1,57 @@
-﻿namespace Alexus.ThinMvvm.Client.ViewModel
+﻿using System;
+using System.Linq;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Alexus.ThinMvvm.Client.ViewModel
 {
-	public class TestCasesViewModel
+	public class TestCasesViewModel: ThinMvvmViewModelBase
 	{
+		private TestCase _currentCase;
 
 		public TestCasesViewModel(TestCaseList cases)
 		{
 			Cases = cases;
 		}
 
-		public object Cases { get; private set; }
+		public TestCaseList Cases { get; private set; }
+
+		public TestCase CurrentCase
+		{
+			get { return _currentCase; }
+			set
+			{
+				if (Equals(value, _currentCase)) return;
+				_currentCase = value;
+				RaisePropertyChanged(() => CurrentCase);
+				NavigateToCase(value);
+			}
+		}
+
+		void NavigateToCase(TestCase target)
+		{
+			var view = ServiceLocator.Current.GetInstance(typeof(Object), target.View);
+
+			var rManager = ServiceLocator.Current.GetInstance<IRegionManager>();
+			NavifateTo(RegionNames.CurrentTestCase, view);
+		}
+
+		public static void ClearRegion(string regionName)
+		{
+			var regionManager = ServiceLocator.Current.GetInstance<IRegionManager>();
+
+			foreach (var view in regionManager.Regions[regionName].Views.ToArray())
+			{
+				regionManager.Regions[regionName].Remove(view);
+			}
+		}
+
+		public static void NavifateTo(string regionName, Object view)
+		{
+			ClearRegion(regionName);
+			var regionManager = ServiceLocator.Current.GetInstance<IRegionManager>();
+			regionManager.AddToRegion(RegionNames.CurrentTestCase, view);
+		}
 	}
 }
